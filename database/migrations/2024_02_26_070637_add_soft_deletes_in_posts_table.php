c47ca6d15a5f624eb2b4e85d9173c5827eeb14c3
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-26 15:30:41 +0800

    add softDeletes migration for posts table

--- database/migrations/2024_02_26_070637_add_soft_deletes_in_posts_table.php
+++ database/migrations/2024_02_26_070637_add_soft_deletes_in_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->softDeletes();
+            $table->softDeletes(); // Add soft deletes to the posts table
         });
     }
 
