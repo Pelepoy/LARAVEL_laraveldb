c60fa0f315b9134f9749fc832c4d947b5e7302c4
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-26 18:03:45 +0800

    add comments in the column

--- database/migrations/2024_02_26_070637_add_soft_deletes_in_posts_table.php
+++ database/migrations/2024_02_26_070637_add_soft_deletes_in_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->softDeletes(); // Add soft deletes to the posts table
+            $table->softDeletes(); // Add soft deletes column to the posts table
         });
     }
 
