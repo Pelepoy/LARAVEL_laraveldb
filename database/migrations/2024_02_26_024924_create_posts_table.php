b4fb9d4e04dae999117d58c695db4944c44bd7c1
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-26 15:28:34 +0800

    initial commit :rocket:

---
+++ database/migrations/2024_02_26_024924_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->from(1000); // Start auto increment from 1000
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->text('excerpt')
+                ->comment('Short description of the post');
+            $table->longText('description');
+            $table->boolean('is_published')->default(false); // Post is not published by default
+            $table->integer('min_to_read')->nullable(false); // Estimated time to read the post
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
