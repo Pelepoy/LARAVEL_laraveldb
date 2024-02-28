e547eba78c90cb6d0a2068d986b24b388c044d98
Philip Marcel Maratas <philipmarcel.maratas.dit.cvsu@gmail.com>
2024-02-28 11:03:53 +0800

    Add foreign key constraint for user_id in posts table

--- database/migrations/2024_02_26_024924_create_posts_table.php
+++ database/migrations/2024_02_26_024924_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id()->from(1000); // Start auto increment from 1000
+            $table->foreignId('user_id')
+                ->comment('Author of the post')
+                ->constrained('users')
+                ->cascadeOnDelete();
             $table->string('title')->unique();
             $table->string('slug')->unique();
             $table->text('excerpt')
